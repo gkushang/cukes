e08089fc161743a3e9b95383711c7f09e5fb6334
kugajjar <kugajjar@paypal.com>
2013-12-06 00:41:37 -0500

    Fetch features from DB, if not then from Local

--- cukesRepo/src/main/java/com/cukesrepo/repository/FeatureRepository.java
+++ cukesRepo/src/main/java/com/cukesrepo/repository/FeatureRepository.java
@@ -1,47 +1,81 @@
 package com.cukesrepo.repository;
 
-import com.cukesrepo.component.FeatureComponent;
 import com.cukesrepo.domain.Feature;
 import org.apache.commons.lang.Validate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class FeatureRepository
 {
 
-    private final String FEATURE_FILE_PATH = "/src/test/resources/features";
-    private final ProjectRepository _projectRepository;
-    private ArrayList<Feature> _features = new ArrayList<Feature>();
-    private FeatureComponent _featureComponent;
+    private final MongoTemplate _mongoTemplate;
+    private final GitRepository _gitRepository;
+    private List<Feature> _features = new ArrayList<>();
+
+    private static final Logger LOG = LoggerFactory.getLogger(FeatureRepository.class);
 
     @Autowired
-    public FeatureRepository(FeatureComponent featureComponent
-    , ProjectRepository projectRepository)
+    public FeatureRepository
+            (
+                    GitRepository gitRepository,
+                    MongoTemplate mongoTemplate
+
+            )
     {
 
-        Validate.notNull(featureComponent, "featureComponent cannot be null.");
-        Validate.notNull(projectRepository, "projectRepository cannot be null.");
+        Validate.notNull(gitRepository, "gitRepository cannot be null");
+        Validate.notNull(mongoTemplate, "mongoTemplate cannot be null");
 
-        _featureComponent = featureComponent;
-
-        _projectRepository = projectRepository;
+        _gitRepository = gitRepository;
+        _mongoTemplate = mongoTemplate;
 
     }
 
-    public ArrayList<Feature>  getFeatures()
+    public List<Feature>  getFeatures()
     {
         return _features;
     }
 
 
-    public ArrayList<Feature> fetch(String projectName)
+    public List<Feature> fetch(String projectName)
     {
-        _features = _featureComponent.fetch(_projectRepository.getProjectByName(projectName).get().getRepositoryPath() + FEATURE_FILE_PATH);
+
+        Query query = new Query(Criteria.where(Feature.PROJECTNAME).is(projectName));
+
+        List<Feature> _features = _mongoTemplate.find(query, Feature.class);
+
+        if(_features == null || _features.size() <= 0)
+        {
+            LOG.info("Fetching features from Git for '{}' project", projectName);
+
+            _features = _gitRepository.fetchFeatures(projectName);
+
+            LOG.info("Inserting features to db for '{}' project", projectName);
+
+            _mongoTemplate.insertAll(_features);
+
+        }else
+        {
+            LOG.info("Features found from db for '{}' project", projectName);
+        }
 
         return _features;
+
+    }
+
+    public void approveScenario(String featureId, String scenarioId)
+    {
+         //TODO - add code for approval
+
     }
 
 }
