ae46748c0c8c6b1f6a3077ea4d47a580281987c5
kugajjar <kugajjar@paypal.com>
2013-12-06 00:37:46 -0500

    Process Feature files to update fields such as totalScenarios, outlines, approved scenarios

--- cukesRepo/src/main/java/com/cukesrepo/component/FeatureComponent.java
+++ cukesRepo/src/main/java/com/cukesrepo/component/FeatureComponent.java
@@ -2,26 +2,15 @@ package com.cukesrepo.component;
 
 
 import com.cukesrepo.domain.Feature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import gherkin.formatter.JSONFormatter;
-import gherkin.parser.Parser;
-import gherkin.util.FixJava;
+import com.cukesrepo.domain.Project;
 import org.apache.commons.lang.Validate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FilenameFilter;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 @Component
 public class FeatureComponent
 {
     private final ScenarioComponent _scenarioComponent;
-    private ArrayList<Feature> features = new ArrayList<Feature>();
-    private final String FEATURE_FILE_EXTENSION = ".feature";
 
     @Autowired
     public FeatureComponent(ScenarioComponent scenarioComponent)
@@ -31,73 +20,19 @@ public class FeatureComponent
         _scenarioComponent = scenarioComponent;
     }
 
-    //this can be replaced by gitHub repo
-    public ArrayList<Feature> fetch(String directoryPath)
-    {
-        features.clear();
-        for(File file : _finder(directoryPath))
-        {
-            Feature feature = _parseFeatureFile(file.getAbsolutePath());
-            feature = processScenarios(feature);
-            features.add(feature);
-            _parseFeatureFile(file.getAbsolutePath());
-        }
 
-        return features;
-    }
-
-    public Feature processScenarios(Feature feature)
+    public Feature processFeature(Project project, Feature feature)
     {
-        feature.setNumberOfScenarios(_scenarioComponent.getNumberOfScenarios(feature));
-        feature.setNumberOfApprovedScenarios(_scenarioComponent.getNumberOfApprovedScenarios(feature));
+        Validate.notNull(feature, "feature cannot be null");
+        Validate.notNull(project, "project cannot be null");
+
+        _scenarioComponent.processScenarios(feature.getScenarios());
+
+        feature.setTotalScenarios(_scenarioComponent.getNumberOfScenarios());
+        feature.setTotalApprovedScenarios(_scenarioComponent.getNumberOfApprovedScenarios());
+        feature.setProjectName(project.getName());
+
         return  feature;
     }
 
-    private File[] _finder(String directoryPath)
-    {
-        File dir = new File(directoryPath);
-
-        return
-                dir.listFiles
-                        (new FilenameFilter()
-                        {
-                            public boolean accept(File dir, String filename)
-                            {
-                                return filename.endsWith(FEATURE_FILE_EXTENSION);
-                            }
-                        }
-                        );
-    }
-
-    private Feature _parseFeatureFile(String path)
-    {
-
-        try
-        {
-            String gherkin = FixJava.readReader(new InputStreamReader(
-                    new FileInputStream(path), "UTF-8"));
-
-            StringBuilder json = new StringBuilder();
-
-            JSONFormatter formatter = new JSONFormatter(json);
-
-            Parser parser = new Parser(formatter);
-
-            parser.parse(gherkin, path, 0);
-
-            formatter.done();
-
-            formatter.close();
-
-            ObjectMapper mapper = new ObjectMapper();
-
-            return mapper.readValue(json.toString(), Feature[].class)[0];
-
-
-        } catch (Exception e)
-        {
-            throw new RuntimeException("\nError in parsing file : " + path, e);
-        }
-
-    }
 }
