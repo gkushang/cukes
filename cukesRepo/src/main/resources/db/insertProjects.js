16fa7bc79904e24076b78a15365d7878ab04c67b
kugajjar <kugajjar@paypal.com>
2013-12-06 09:05:18 -0500

    insert Projects to DB - JS

--- cukesRepo/src/main/resources/db/insertProjects.js
+++ cukesRepo/src/main/resources/db/insertProjects.js
@@ -1,19 +1,20 @@
 var project.one = "oraTest";
 var project.two = "campaignManagerTest";
+var workspace =  "/Users/kugajjar/Documents/workspace/";"
 
 var projects = [
                     	{
                     		"_class" : "com.cukesrepo.domain.Project",
                     		"name" : project.one,
-                    		"repositorypath" : "/Users/kugajjar/Documents/workspace/oraTest"
+                    		"repositorypath" : workspace+project.one
                     	},
                     	{
                     		"_class" : "com.cukesrepo.domain.Project",
                     		"name" : project.two,
-                    		"repositorypath" : "/Users/kugajjar/Documents/workspace/campaignManagerTest"
+                    		"repositorypath" : workspace+project.two
                     	}
                     ]
 
 db.project.remove({"name" : project.one});
 db.project.remove({"name" : project.two});
-db.project.insert(projects);
+db.project.insertAll(projects);
