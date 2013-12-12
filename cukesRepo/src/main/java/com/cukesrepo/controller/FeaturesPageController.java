f38ea77afe8a92706ce1559bf8f81e8a9dd5fff9
maduraisamy <maduraisamy@paypal.com>
2013-12-11 21:30:29 -0500

    css and email dynamic

--- cukesRepo/src/main/java/com/cukesrepo/controller/FeaturesPageController.java
+++ cukesRepo/src/main/java/com/cukesrepo/controller/FeaturesPageController.java
@@ -1,56 +1,51 @@
 package com.cukesrepo.controller;
 
-import com.cukesrepo.exceptions.FeatureNotFoundException;
+
 import com.cukesrepo.exceptions.ProjectNotFoundException;
+import com.cukesrepo.page.FeaturesPage;
+
+import com.cukesrepo.service.email.EmailService;
 import com.cukesrepo.service.feature.FeatureService;
 import com.cukesrepo.service.project.ProjectService;
 import org.apache.commons.lang.Validate;
+import org.rendersnake.HtmlCanvas;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
 
 @Controller
 public class FeaturesPageController {
 
-    private final ProjectService _projectService;
-    private final FeatureService _featureService;
+    public FeatureService _featureService;
+    public ProjectService _projectService;
+    public EmailService _emailService;
+
 
     @Autowired
-    public FeaturesPageController
-            (
-                    ProjectService projectService,
-                    FeatureService featureService
-            ) {
 
-        Validate.notNull(projectService, "projectService cannot be null");
+    public FeaturesPageController(FeatureService featureService,ProjectService projectService,EmailService emailService) {
+
         Validate.notNull(featureService, "featureService cannot be null");
 
-        _projectService = projectService;
         _featureService = featureService;
-    }
+        _projectService = projectService;
+        _emailService=emailService;
 
-    @RequestMapping(value = {"/projects/{projectName}/"}, method = RequestMethod.GET)
-    protected ModelAndView featuresPage
-            (
-                    @PathVariable String projectName
+}
 
-            ) throws FeatureNotFoundException, ProjectNotFoundException {
+    @RequestMapping(value = {"/projects/{projectName}/"})
+    @ResponseBody
+    public void featurespage(HtmlCanvas html, @PathVariable String projectName) throws IOException, ProjectNotFoundException {
 
-        ModelAndView model = new ModelAndView("FeaturesPage");
+        html.render(new FeaturesPage(_featureService,_projectService,_emailService,_projectService.getProjectByName(projectName)));
 
-        try {
-            model.addObject("features", _featureService.fetchFeatures(_projectService.getProjectByName(projectName)));
+   
 
-        } catch (FeatureNotFoundException fe) {
 
-        } catch (ProjectNotFoundException pe) {
-
-        }
-
-        return model;
     }
 
 
